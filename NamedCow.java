5d8bcc7951b402dc061d8de922bb81d959a240d0
Mariko <Chi@Katherines-MacBook-Air.local>
2018-10-30 08:27:32 -0700

    fixed initializations

--- NamedCow.java
+++ NamedCow.java
@@ -2,7 +2,12 @@ class NamedCow extends Cow
 {
   private String myName;
   public NamedCow(String type, String sound, String name) {
+    myType = type;
     myName = name;
+    mySound = sound;
+  }
+  public NamedCow() {
+    myName = "unknown";
   }
   public String getName() {
     return myName;
